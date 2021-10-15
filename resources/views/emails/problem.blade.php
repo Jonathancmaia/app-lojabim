594d79a5bcffb0f555872a0aa9526f725295c6dd
jonathancmaia <jonathancmaia@gmail.com>
2021-10-15 07:03:45 -0300

    formulação de dados para envio de email de prblema pt6

--- resources/views/emails/problem.blade.php
+++ resources/views/emails/problem.blade.php
@@ -1,1 +1,11 @@
-<h1>{{ $email }}<h1>
+<p>O seguinte cliente possui um problema:</p>
+<h1>{{ $nome }}<h1>
+<br/>
+<br/>
+<p>informações de contato</p>
+<p>{{ $telefone }}<p>
+<p>{{ $email }}<p>
+<br/>
+<br/>
+<p>Relato do problema</p>
+<h2>{{ $problema }}<h2>
