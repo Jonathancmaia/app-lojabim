d607120362810dc4a27167f37f333a05ad45571a
jonathancmaia <jonathancmaia@gmail.com>
2022-01-03 15:44:55 -0300

    novas configurações cors

--- config/cors.php
+++ config/cors.php
@@ -17,11 +17,11 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization'],
+    'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => false,
